318b9b41a46b4a618c4ce59b0074798955e1642b
viniiXx <fernadesvinicius7298@gmail.com>
2025-04-06 18:02:56 -0300

    novo estudo

---
+++ src/matriz/application/Matriz.java
@@ -0,0 +1,21 @@
+package matriz.application;
+
+public class Matriz {
+    public static void main(String[] args) {
+        int[] arrNum = {1, 2, 3};
+        int [][] arrInt = new int[3][];
+
+        arrInt[0] = new int[2];
+        arrInt[1] = arrnum;
+        arrInt[2] = new int[]{1,2,3,4,5,6};
+
+        int[][] arrInt2 = {{0,0}, {0,1,2}, {1,2,3,4,5,6}};
+
+        for (int[] arrBase : arrInt2){
+            System.out.println("\n");
+            for (int num : arrBase){
+                System.out.print(num + "");
+            }
+        }
+    }
+}
---
+++ src/matriz/application/MatrizForEach.java
@@ -0,0 +1,26 @@
+package matriz.application;
+
+public class MatrizForEach {
+    public static void main(String[] args) {
+        int [][] dias = new int [3][3];
+        dias [0][0] = 31;
+        dias [0][1] = 30;
+        dias [0][2] = 29;
+
+        dias [1][0] = 28;
+        dias [1][1] = 29;
+        dias [1][2] = 30;
+
+        for (int i = 0; i < dias.length ; i++){
+            for (int j = 0; j < dias[i].length; j++){
+                System.out.println(dias[i][j]);
+            }
+        }
+
+        for (int[] arrBase : dias){
+            for (int num : arrBase){
+                System.out.println(num);
+            }
+        }
+    }
+}
