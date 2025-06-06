ead6304b596bbb8f09265537ef8269cff7b137dc
viniiXx <fernadesvinicius7298@gmail.com>
2025-06-06 00:52:08 -0300

    novo exercicio variaveis

---
+++ src/introducao/variaveis/ex002.java
@@ -0,0 +1,10 @@
+package introducao.variaveis;
+
+public class ex002 {
+    public static void main(String[] args) {
+        int quantidadeFarinha = 10;
+        int quantidadeAcucar = 5;
+        System.out.println("quantidade de farinha: " + quantidadeFarinha);
+    System.out.println("quantidade de açucar: " + quantidadeAcucar);
+    }
+}
