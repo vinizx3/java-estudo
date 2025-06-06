0ea6553180f2e41ffa5c27bf22875a9a80bb385c
viniiXx <fernadesvinicius7298@gmail.com>
2025-06-05 21:10:37 -0300

    alteração no codigo

---
+++ src/poo/classes/application/Program.java
@@ -0,0 +1,5 @@
+package poo.classes;
+
+public class Program {
+    
+}
---
+++ src/poo/classes/entities/Person02.java
@@ -0,0 +1,7 @@
+package poo.classes.entities;
+
+public class Person {
+    public String nome;
+    public int idade;
+    public char sexo;
+}
