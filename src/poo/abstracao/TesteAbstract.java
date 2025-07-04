52291eb1900faa0d648088f37e851f8a77b63aeb
vinicius <fernadesvinicius7298@gmail.com>
2025-07-03 22:27:38 -0300

    Aprendendo classes abstratas

--- src/poo/abstracao/TesteAbstract.java
+++ src/poo/abstracao/TesteAbstract.java
@@ -1,4 +1,33 @@
 package poo.abstracao;
+abstract class Veiculo{
+    int codigo = 35;
+
+    Veiculo(int cod){
+        codigo = cod;
+    }
+
+    abstract void acelerar();
+
+}
+class Caminhao extends Veiculo{
+    Caminhao(int codigo){
+        super(codigo);
+    }
+    void acelerar(){
+        System.out.println("O caminhão está acelerando");
+    }
+    void freiar(){
+        System.out.println("o caminhão está freiando");
+    }
+}
 
 public class TesteAbstract {
+    public static void main(String[] args) {
+        Caminhao caminhao = new Caminhao(777);
+
+        System.out.println("Codigo: " + caminhao.codigo);
+
+        caminhao.acelerar();
+        caminhao.freiar();
+    }
 }
