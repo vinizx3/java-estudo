a0bc572c165c0f77951d562eb1e0a26d5f87ccce
vinicius <fernadesvinicius7298@gmail.com>
2025-07-04 21:09:54 -0300

    Polimorfismo em interfaces

--- src/poo/polimorfismo/InterfacePolimorfismo.java
+++ src/poo/polimorfismo/InterfacePolimorfismo.java
@@ -1,4 +1,31 @@
 package poo.polimorfismo;
+interface Veiculo{
+    void acelerar();
+    void drift();
+}
+class Civic implements Veiculo{
+    @Override
+    public void acelerar() {
+        System.out.println("O civic está acelerando");
+    }
+    public void drift(){}
+}
+class Bmw implements Veiculo{
+    @Override
+    public void acelerar() {
+        System.out.println("a bmw está acelerando");
+    }
+    public void drift(){
+        System.out.println("A bmw está fazendo drift");
+    }
+}
 
 public class InterfacePolimorfismo {
+    public static void main(String[] args) {
+        Veiculo meuCivic = new Civic();
+        Veiculo minhaBmw = new Bmw();
+
+        meuCivic.acelerar();
+        minhaBmw.drift();
+    }
 }
